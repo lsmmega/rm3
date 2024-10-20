83f6d4988a3c6d33f5fcf4a1b8a15f21a7cda7f9
lsmmega <pogmegalsm@outlook.com>
2024-10-20 11:12:19 +0800

    create blank tank

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 bmp2nes gfx/obj/capsule/large_life_capsule.bmp
 bmp2nes gfx/obj/capsule/small_life_capsule.bmp
+bmp2nes gfx/obj/tank/blank_tank.bmp
 bmp2nes gfx/obj/capsule/small_wpn_capsule.bmp
 bmp2nes gfx/obj/tank/e_tank.bmp
 bmp2nes gfx/obj/killed.bmp
