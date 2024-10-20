37b85fb31a1d1d33d1015ea4214915f7951053b9
lsmmega <pogmegalsm@outlook.com>
2024-10-20 08:21:23 +0800

    create 1up

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ bmp2nes gfx/obj/capsule/small_wpn_capsule.bmp
 bmp2nes gfx/obj/tank/e_tank.bmp
 bmp2nes gfx/obj/killed.bmp
 bmp2nes gfx/obj/bar.bmp
+bmp2nes gfx/obj/1up.bmp
 bmp2nes gfx/obj/capsule/large_wpn_capsule.bmp
 bmp2nes gfx/obj/wpn/mega_buster.bmp
 asm6 main.asm rm3.nes
